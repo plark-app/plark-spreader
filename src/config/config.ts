ed545a6a22f4bd8e3f683c6cc2d1dae99087b8f9
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2018-09-20 15:22:14 +0300

    Added Firebase configuration and remove .env.yml

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { ConfigRegistry } from './config-registry';
+import { ConsoleColor } from 'common/console';
 
 const config = new ConfigRegistry<ApplicationConfig>();
 const rootPath = process.cwd();
@@ -8,9 +9,15 @@ const configPath = path.resolve(rootPath, './config/');
 config.loadYmlFile(path.resolve(configPath, 'app.yml'));
 config.loadYmlFile(path.resolve(configPath, 'database.yml'));
 config.loadYmlFile(path.resolve(configPath, 'tracker.yml'));
+config.loadYmlFile(path.resolve(configPath, 'firebase.yml'));
 
 // Loading .env.yml file from root repository
-config.loadYmlFile(path.resolve(rootPath, '.env.yml'));
+try {
+    const envConfig = path.resolve(rootPath, '.env.yml');
+    config.loadYmlFile(envConfig);
+} catch (error) {
+    console.warn(`${ConsoleColor.FgYellow}  Provide .env.yml file!  ${ConsoleColor.Reset}`);
+}
 
 
 export { ConfigRegistry, config };
